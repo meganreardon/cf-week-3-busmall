60c5f8953552afb97d85d251bf504eddd47ce6d1
Megan Reardon <meganr@gmail.com>
2016-05-23 16:36:54 -0700

    working out getting 3 random numbers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // what we have to start with
-var theImageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var theImageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
+// note: need to get the USB gif and sweep png back in there
 
 // global variables
 var theProducts = [];
@@ -36,19 +37,31 @@ theContainer.innerHTML = '<img src=' + theProducts[2].imgPath + ' />' + '<img sr
 // var minNumber = 0;
 // var maxNumber = theProducts.length - 1; // fix this with thing mentioned in class
 
+var randomNumber = 0;
+
 function makeMyRandomNumber() {
   var min = 0;
   var max = theProducts.length - 1;
-  var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   return randomNumber;
   // console.log(randomNumber);
 }
 
+// get three different random numbers
+
+function threeRandomNumbers () {
+  // for loop to get three random numbers
+  // check to see if new random number is the same as any current random numbers
+  // these three numbers will make the three images
+};
+
+threeRandomNumbers();
+
 function randomImages() {
-  var gimmeRandomNumber = makeMyRandomNumber();
-  console.log(gimmeRandomNumber);
+  makeMyRandomNumber();
+  // console.log(randomNumber);
   // render a random image on the page
-  theContainer.innerHTML = '<img src=' + theProducts[gimmeRandomNumber].imgPath + ' />';
+  theContainer.innerHTML = '<img src=' + theProducts[randomNumber].imgPath + ' />';
 };
 
 randomImages();
