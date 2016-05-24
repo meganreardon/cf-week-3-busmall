db3cf711455c498efebf922f98f6a0ac9f6ac75a
Megan Reardon <meganr@gmail.com>
2016-05-23 18:50:56 -0700

    have user clicks + and - working

--- app.js
+++ app.js
@@ -27,17 +27,6 @@ function callCreateTheProducts() {
 // creating the objects
 callCreateTheProducts();
 
-// put an image inside of the container
-
-// TURN ME BACK ON IF NEEDED
-// theContainer.innerHTML = '<img src=' + theProducts[2].imgPath + ' />' + '<img src=' + theProducts[3].imgPath + ' />';
-
-// create a random number
-
-// create min and max vaules and random number
-// var minNumber = 0;
-// var maxNumber = theProducts.length - 1; // fix this with thing mentioned in class
-
 var randomNumber = 0;
 
 function makeMyRandomNumber() {
@@ -45,62 +34,50 @@ function makeMyRandomNumber() {
   var max = theProducts.length - 1;
   randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   return randomNumber;
-  // console.log(randomNumber);
 }
 
-// get three different random numbers
-
-// function threeRandomNumbers () {
-//   // for loop to get three random numbers
-//   // check to see if new random number is the same as any current random numbers
-//   // these three numbers will make the three images
-// };
-//
-// threeRandomNumbers();
-
-function randomImages() {
-  makeMyRandomNumber();
-  // console.log(randomNumber);
-  // render a random image on the page
-  theContainer.innerHTML = '<img src=' + theProducts[randomNumber].imgPath + ' />';
-};
-
-// loop
+// get three random numbers
 function getThreeRandomNumbers () {
-  var randomOne = makeMyRandomNumber();
-  var randomTwo = makeMyRandomNumber();
+  randomOne = makeMyRandomNumber();
+  randomTwo = makeMyRandomNumber();
   while (randomTwo === randomOne) {
     randomTwo = makeMyRandomNumber();
   };
-  var randomThree = makeMyRandomNumber();
+  randomThree = makeMyRandomNumber();
   while ((randomThree === randomOne) || (randomThree === randomTwo)) {
     randomThree = makeMyRandomNumber();
   }
-  console.log(randomOne + ' : ' + randomTwo + ' : ' + randomThree);
+  // console.log(randomOne + ' : ' + randomTwo + ' : ' + randomThree);
+  return randomOne;
+  return randomTwo;
+  return randomThree;
 }
 
 getThreeRandomNumbers();
 
-  // make number
-  // if - first number then go against
-  // if - second number then check against fist number and go again -- if is same i--
-  // if - third number check against first and second, if is same i-- and go again
-  // check to see if number is equal
+function randomImages() {
+  getThreeRandomNumbers();
+  theContainer.innerHTML = '<img src=' + theProducts[randomOne].imgPath + ' /><img src=' + theProducts[randomTwo].imgPath + ' /><img src=' + theProducts[randomThree].imgPath + ' />';
+};
 
-// TURN ME BACK ON
+// turned on for testing, otherwise will be inside the event handler function
 // randomImages();
-
-// var thElement = document.createElement('th');
-// thElement.textContent = Math.ceil(perDayTotal);
-// trElement.appendChild(thElement);
-
-// put a random image up on the page inside of theContainer
 
 // event handler
 theContainer.addEventListener('click', handleContainer);
 
 function handleContainer(event) {
   console.log(event.target.id); // this will return which one you clicked on
+  // if click is on container total click cound i--
+  if (event.target.id === 'thecontainer') {
+    // alert('container!');
+    userClicksTotal--;
+    console.log(userClicksTotal);
+  } else {
+    userClicksTotal++;
+    console.log(userClicksTotal);
+  }
+  randomImages();
 }
 
 // write full event handler, call three new random images at bottom of it
