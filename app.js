072ea7f93d887d0c337f0ba5f768d2c937769f5b
Megan Reardon <meganr@gmail.com>
2016-05-23 20:19:32 -0700

    all tracking working, rough code

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ function callCreateTheProducts() {
 // creating the objects
 callCreateTheProducts();
 
+// random number makeMyRandomNumber
 function makeMyRandomNumber() {
   var min = 0;
   var max = theProducts.length - 1;
@@ -47,17 +48,12 @@ function getThreeRandomNumbers () {
   while ((randomThree === randomOne) || (randomThree === randomTwo)) {
     randomThree = makeMyRandomNumber();
   }
-  // console.log(randomOne + ' : ' + randomTwo + ' : ' + randomThree);
-  return randomOne;
-  return randomTwo;
-  return randomThree;
 }
 
-getThreeRandomNumbers();
-
+// render three images and add to times displayed
 function randomImages() {
   getThreeRandomNumbers();
-  theContainer.innerHTML = '<img src=' + theProducts[randomOne].imgPath + ' id= ' + randomOne + ' /><img src=' + theProducts[randomTwo].imgPath + ' id= ' + randomTwo + ' /><img src=' + theProducts[randomThree].imgPath + ' id= ' + randomThree + ' />';
+  theContainer.innerHTML = '<img src=' + theProducts[randomOne].imgPath + ' id=' + randomOne + ' /><img src=' + theProducts[randomTwo].imgPath + ' id=' + randomTwo + ' /><img src=' + theProducts[randomThree].imgPath + ' id=' + randomThree + ' />';
   theProducts[randomOne].timesDisplayed ++;
   theProducts[randomTwo].timesDisplayed ++;
   theProducts[randomThree].timesDisplayed ++;
@@ -70,17 +66,16 @@ theContainer.addEventListener('click', handleContainer);
 
 function handleContainer(event) {
   if (upToTwentyFive === false) {
-    // console.log(event);
-    // console.log(event.target.id); // this will return which one you clicked on
     if (userClicksTotal === 24) {
-      // this console logs the last click twice, which is why I've set it to 24
-      // not to self: figure out why
+      // set to 24 because it enters the loop one last time
+      // note to self: fix this later
       upToTwentyFive = true;
     } else if (event.target.id === 'thecontainer') {
       userClicksTotal--;
     } else {
       userClicksTotal++;
-      console.log(event.target.id);
+      // console.log(userClicksTotal);
+      // console.log(event.target.id);
       var thisid = parseInt(event.target.id);
       theProducts[thisid].timesClicked ++;
     }
